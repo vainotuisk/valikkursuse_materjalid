266d630ce47ed522c3223e28cee4f43fc9c80e70
Väino Tuisk <vainotuisk@gmail.com>
2014-01-04 15:14:27 +0200

    3. videoloengu näited

---
+++ kordused.py
@@ -0,0 +1,36 @@
+﻿##
+# Väino Tuisk        
+# Kordused for ja while
+# Videoloengu näited.
+
+# Näide 1 - järjendiks täisarvud
+for i in [1,2,3,4]:
+    print("i väärtus on " + str(i))
+
+    
+# Näide 2 - järjendiks sõned
+##for i in ["Esimene","teine", "kolmas"]:
+##    print("i väärtus on " + i))
+
+# Näide 3 - funktsiooni range kasutamine
+##for i in range(5):
+##    print("i väärtus on " + str(i))
+
+# Näide 4 - funktsiooni range kasutamine kolme argumendiga
+##for i in range(10,1,-1):
+##    print(i*"*")
+
+# Näide 5 - topelttsükkel
+##for i in range(3):
+##    for j in range(5):
+##        print(i),
+##        print(j)
+
+# Näide 6 - while tsükkel
+##korrata = True
+##arv =10
+##while i:
+##    print(arv)
+##    arv -=1 # arv = arv - 1
+##    if (arv == 0):
+##        korrata = False
