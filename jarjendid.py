0d27cb9254ea477a9009d9d91219a317525d3920
Väino Tuisk <vainotuisk@gmail.com>
2014-01-05 21:34:32 +0200

    Loengu näited

---
+++ jarjendid.py
@@ -0,0 +1,26 @@
+﻿##
+# Väino Tuisk        
+# Järjend
+sona = "ABCdef"
+print sona[2]
+print sona[2:]
+print sona[:2]
+
+##
+##jarjend = ["sõne",True,12,3.5] # järjendis võivad olla erinevat tüüpi elemendid
+##for i in jarjend:
+##    print (i)
+
+###järjendissse lisamine
+##minu_jarjend = [2,4,5,6]
+##print(minu_jarjend)
+##minu_jarjend.append(9)
+##print(minu_jarjend)
+
+### järjendi moodustamine ja konsooli sisendi kaudu elementide lisamine
+##minu_jarjend = [] # tühi järjend
+##for i in range(5):
+##sisend = input( "Sisesta täisarv: ")
+##sisend = int( sisend )
+##minu_jarjend.append(sisend)
+##print(minu_jarjend)
