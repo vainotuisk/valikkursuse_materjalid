271247080aca8f725c5f6032fdd59487855b276a
Väino Tuisk <vainotuisk@gmail.com>
2014-01-06 11:51:01 +0200

    Tekst sisestatud, vaja lisada ja kontrollida programmi kood

--- tekstimang_LOSS.py
+++ tekstimang_LOSS.py
@@ -17,25 +17,53 @@ ruum = ['Sa oled fuajees. Siin on uks põhja ja itta.', 1, 1, None, None]
 ruumid.append(ruum)
 # Korda viimaseid tegevusi kõigi ruumide jaoks, mida soovid mängus kasutada.
 
-##    Create a variable called current_room. Set it to zero.
+
 # Tee muutuja asukoht ja sea selle väärtuseks 0.
 asukoht = 0
 
 ##  Kasutades muutujaid asukoht ja ruumid, väljasta konsoolile selle ruumi andmed, kus parasjagu oled. Tulemus peaks olema midagi sellist:
 ##  ['Sa oled fuajees. Siin on uks põhja ja itta.', 1, 1, None, None]
-print(ruumid[asukoht+1])
-lokk=input("mis")
+print(ruumid[asukoht])
 ##Muuda väljundit selliselt, et väljastatakse ainult tekst ruumi kohta, mitte järjendi teisi andmeid, nt:
 ##    "Sa oled fuajees. Siin on uks põhja ja itta."
-##    Create a variable called done and set it to False. Then put the printing of the room description in a while loop that repeats until done is set to True.
-##    After printing the room description, add a line of code that asks the user what direction they wish to go.
+print(ruumid[asukoht][0])
+
+##Tee muutuja tehtud ja anna sellele väärtuseks False
+tehtud = False
+
+##Seejärel pane ruumikirjelduse väljastamine while tsüklisse, mis toimib kuni tehtud saab väärtuseks True
+while tehtud:
+    print(ruumid[asukoht][0])
+
+##    Peale ruumi kirjelduse väljastamist, lisa rida, mis küsib kasutajalt mis suunas ta soovib liikuda
+    suund = input("Mis suunas soovid edasi liikuda? (n/e/s/w) "
 ##    Add an if statement to see if the user wants to go north.
-##    If the user wants to go north, create a variable called next_room and get it equal to room_list[current_room][1], which should be the number for what room is to the north.
-##    Add another if statement to see if the next room is equal to None. If it is, print “You can't go that way.” Otherwise set current_room equal to next_room.
-##    Test your program. Can you go north to a new room?
-##    Add elif statements to handle east, south, and west. Add an else statement to let the user know the program doesn't understand what she typed.
-##    Add several rooms, at least five. It may be necessary to draw out the rooms and room numbers to keep everything straight. Test out the game. You can use \n or triple quotes if you have a multi-line room description.
-##    Optional: Add a quit command. Make sure that the program works for upper and lower case directions. Have the program work if the user types in “north” or “n”. 
+## lisa if-lause, mis kontrollib kas valiti põhjasuund (n)
+    if suund == "n":
+##    If the user wants to go north, create a variable called next_room and get it equal to room_list[current_room][1],
+##                  which should be the number for what room is to the north.
+##    Kui kasutaja soovib suunduda põhja (n) siis tee muutuja jargmine_ruum sea see võrduma ruumid[asukoht][1]] mis on põhjasuuna ruumi tähis
+        jargmine_ruum = ruumid[asukoht][1]]
+
+##    Lisa veel üks if-lause, mis kontrollib kas jargmine_ruum on võrdne None. Kui see on nii siis prindi välja midagi selllist: "Sa ei saa selles suunas minna".
+        if jargmine_ruum == "None":
+            print("Sa ei saa selles suunas minna.")
+            
+##    Muul juhul sea asukoht võrduma jargmine_ruum väärtusega
+        else:
+            asukoht = jargmine_ruum
+
+##  Testi oma programmi, kas saad minna uude ruumi?            
+
+##  Lisa elif-laused ida-, lõuna- ja läänesuuna valikuks. Samuti lisa else-lause juhuks kui programm ei saa aru, mis märk sisestati.           
+            
+
+##    Lisa ruume juurde, vähemalt 5. Vajadusel tee joonis, et saada ülevaade ruumide asetusest
+    
+##  Testi mängu
+## Arendusvõimalusi:
+##  Lisa mängust väljumise valik, lisa võimalus, et programm töötaks ka suurte tähtedega            
+
 
 
 
